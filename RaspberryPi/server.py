daaba000a801ff096e65511b8f075b4038003307
pedroescalfonimoraes <47799147+pedroescalfonimoraes@users.noreply.github.com>
2019-06-16 12:27:14 -0300

    Update server.py

--- RaspberryPi/server.py
+++ RaspberryPi/server.py
@@ -1,13 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def start_game():
     return "Bem-vindo!"
-
-@app.route("/clock")
-def clock():
-    return "Time is running!"
-
+    gc = gameController()
+    gc.module1()
+    gc.module2()
+    gc.module3()
 
 app.run(port=5000, debug=False)

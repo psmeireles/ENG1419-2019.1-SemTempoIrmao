dd5c16524fa0635b68442c02f73df8a75303c3dc
Fernando Homem da Costa <nandohdc@gmail.com>
2019-07-06 18:40:40 -0300

    db test python

--- PythonServer/db_teste.py
+++ PythonServer/db_teste.py
@@ -1,4 +1,5 @@
 from database_class import *
 db_client = database_client()
 dados = {"date": "date", "time": {"minutes": 5, "seconds":10}, "challenges":[{"name": "teste","time_issued":"teste","params":[],"completed":"true"}],"finished":"true"}
+db_client.connect()
 db_client.send(dados)
